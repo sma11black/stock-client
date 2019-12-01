3c37d623981c50a45447afeb88bb1d87ce4ec7e2
司马黑 <653014496@qq.com>
2019-12-01 16:53:56 +0800

    Auto-configuration for Shared Beans.

--- stock-ui/src/main/java/com/example/stockui/ChartController.java
+++ stock-ui/src/main/java/com/example/stockui/ChartController.java
@@ -1,5 +1,6 @@
 package com.example.stockui;
 
+import com.example.stockclient.WebClientStockClient;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import org.springframework.stereotype.Component;
@@ -9,4 +10,9 @@ public class ChartController {
 
     @FXML
     public LineChart<String, Double> chart;
+    private WebClientStockClient webClientStockClient;
+
+    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }
 }
