906123978928da90ff18df957f9be475908d36b9
司马黑 <653014496@qq.com>
2019-12-03 21:07:09 +0800

    Subscribing Multiple Consumers.

--- stock-ui/src/main/java/com/example/stockui/StockUiApplication.java
+++ stock-ui/src/main/java/com/example/stockui/StockUiApplication.java
@@ -1,7 +1,6 @@
 package com.example.stockui;
 
 import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
